7054157de5d41a255716f7366b902adb54dff8d3
jonathanj20 <jonathanpossada@gmail.com>
2025-09-05 22:11:16 -0700

    new font was added

--- src/components/Imagotype.tsx
+++ src/components/Imagotype.tsx
@@ -2,7 +2,7 @@ export default function Imagotype(){
     return(
         <div className="flex items-center gap-5">
             <img src="./logo.png" alt="logo_el-paceño" className="w-[65px]" title="El paceño"/>
-            <h2 className="text-[#F58811]">Carnitas y Birria <br /> "EL PACEÑO"</h2>
+            <h2 className="text-mainOrange font-kavoon">Carnitas y Birria <br /> "EL PACEÑO"</h2>
         </div>
     )
 }
