e951260d90f0ebde7b9239aa57d57811565faf64
jonathanj20 <jonathanpossada@gmail.com>
2025-09-05 21:51:21 -0700

    Imagotype component was added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,16 @@
 import { useState } from "react";
 import Hamburger from "hamburger-react";
+import Imagotype from "./Imagotype";
 
 export default function Header(){
     const [showNav, setShowNav] = useState(false);
-    const linksNav = ["Inicio", "Promociones", "Ubicación"];
+    const linksNav: string[] = ["Inicio", "Promociones", "Ubicación"];
     const toggleNav = (toggled: boolean) => toggled ? setShowNav(true) : setShowNav(false);
 
     return(
         <header className="flex justify-between p-3">
-            <div className="flex items-center gap-5">
-                <img src="./logo.png" alt="logo_el-paceño" className="w-[65px]" title="El paceño"/>
-                <h2 className="text-[#F58811]">Carnitas y Birria <br /> "EL PACEÑO"</h2>
-            </div>
-            <div className="lg:hidden">
+            <Imagotype/>
+            <div className="flex items-center lg:hidden">
                 <Hamburger onToggle={toggleNav} />
             </div>
             <nav className={`${showNav ? "block" : "hidden"} absolute top-30 flex flex-col items-center gap-5 w-full lg:static lg:w-2/6 lg:flex lg:flex-row lg:justify-evenly`}>
