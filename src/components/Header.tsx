ef9e14ba1259a3ff43528fa65d82d473e1b1be2b
jonathanj20 <jonathanpossada@gmail.com>
2025-10-09 12:32:34 -0700

    nav is closed when user click a link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,17 +13,16 @@ export default function Header(){
         {name: "Promociones", href: "#promotion"},
         {name: "Ubicación", href: "#location"}
     ];
-    const toggleNav = (toggled: boolean) => toggled ? setShowNav(true) : setShowNav(false);
-
+    
     return(
         <header className="sticky top-0 z-10 bg-[#FFFFFA] flex justify-between items-center p-3">
             <Imagotype/>
             <div className="flex items-center lg:hidden">
-                <Hamburger onToggle={toggleNav} />
+                <Hamburger toggled={showNav} toggle={setShowNav} />
             </div>
             <nav className={`${showNav ? "top-30" : "top-[-1000px]"} bg-[#FFFFFA] transition-all duration-[650ms] fixed z-[-1] flex flex-col items-center gap-5 w-full h-[100%] lg:static lg:w-2/6 lg:flex lg:flex-row lg:justify-evenly pt-20 lg:pt-0`}>
                 {
-                    linksNav.map(({name, href}) => <a href={href} className="text-lowGray hover:text-mainOrange transition-all delay-150" key={name}>{name}</a>)
+                    linksNav.map(({name, href}) => <a href={href} onClick={() => setShowNav(false)} className="text-lowGray hover:text-mainOrange transition-all delay-150" key={name}>{name}</a>)
                 }
             </nav>
         </header>
